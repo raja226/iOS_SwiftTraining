1266b1a779a883779d3e66e14ba900be7dd6d621
raja226 <iosrajasekhar@gmail.com>
2024-03-19 13:48:14 +0530

    Print Statemens

--- Mypractice.playground/Contents.swift
+++ Mypractice.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
-
+import OSLog
 var greeting = "Hello, playground"
+
+//Print Staments :
+
+print("Hi Kanini Playground")
+
+debugPrint("Debug message")
+let message = "Hi"
+
+let number = 42
+debugPrint("Number dubug:",number)
+
+struct Person {
+    var name:String
+    var  id:Int
+    
+}
+let person = Person(name: "Rajasekhar", id: 226)
+debugPrint("Person Object:",person)
+print(person)
+debugPrint(#file)
+debugPrint("My file:",#file, "line no:",#line, "function:",#function)
+
