8aea7603260d240a9bf8e5d32db646f072a11121
raja226 <iosrajasekhar@gmail.com>
2024-03-30 17:03:19 +0530

    examples

--- Mypractice.playground/Contents.swift
+++ Mypractice.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 import OSLog
+import Foundation
 var greeting = "Hello, playground"
 
 //Print Staments :
@@ -576,3 +577,345 @@ default:
 
 //compund cases:
 
+switch character
+{
+case "a" ,"A":
+    print("case a = \(character)")
+case "b" , "B":
+    print("case b = \(character)")
+default:
+    print("case default = \(character)")
+
+}
+
+//Fallthrough: what is fallthrough
+/*
+ In Swift, the fallthrough keyword is used within a switch statement to transfer control from one case to the next one without exiting the switch block. When fallthrough is encountered within a case, the execution flow continues to the next case block, regardless of whether the condition in that case evaluates to true or not.
+ */
+switch character
+{
+case "a" ,"A", "C", "c":
+    print("case a fallthrough= \(character)")
+    fallthrough
+case "b" , "B":
+    print("case b = \(character)")
+case  "D":
+    print("case d= \(character)")
+default:
+    print("case default = \(character)")
+
+}
+
+
+//Functions :
+
+//Functions,parameters,retuntype
+
+func countNumbers(input: Int , input2: Int) -> Int {
+    let result = input + input2
+    return result
+}
+let sum = countNumbers(input: 10, input2: 5)
+print(sum)
+var sumresult = 0
+for item in oddNumbers
+{
+    sumresult = sumresult + item
+}
+
+print(sumresult)
+
+//Multiple retun values [e.g. tuples]
+
+func findMinandMax(listofArayInt: [Int]) -> (minvalue :Int , maxValue: Int)
+{
+    var minValue = listofArayInt[0]
+    var maxValue = listofArayInt[0]
+    for number in listofArayInt {
+        if number > maxValue {
+            maxValue = number
+        }
+        if number < minValue {
+            minValue = number
+        }
+    }
+    return (minValue,maxValue)
+}
+
+let toupleresult = findMinandMax(listofArayInt: [1,2,3,4,5,6])
+
+print(toupleresult.minvalue)
+print(toupleresult.maxValue)
+
+//Impliciti return
+
+func multipleNumbers(num1:Int ,num2:Int) -> Int {
+    num1 * num2
+}
+
+print(multipleNumbers(num1: 10, num2: 2))
+
+//argument labels and paramenter names
+
+func greeetPerson(with name: String) {
+    
+    print("hi \(name)")
+}
+greeetPerson(with: "Alice")
+//greeetPerson(name: "Alice")
+
+
+//variadic parameters
+func caluculateAverage(withInput numbers: Double...) -> Double
+{
+    var avg = 0
+    var sum:Double = 0
+    var totalcount = 0
+    for number in numbers
+    {
+        sum += number
+        totalcount += 1
+        
+    }
+    print(Double(sum / Double(totalcount)))
+    return Double(sum / Double(totalcount))
+}
+
+caluculateAverage(withInput:1,2,3,4,5,6)
+
+//in-out parameters
+/*
+ inout is a keyword used to specify that a parameter can be modified by a function and those modifications are reflected outside the function's scope.
+ */
+func greeting(person: inout String)
+{
+    person = "Bob"
+}
+var nameparam = "Rajasekhar"
+print(nameparam)
+greeting(person: &nameparam)
+print(nameparam)
+
+
+
+
+//Defer keyword:
+
+sample()
+
+func sample() {
+   defer {
+       print("One")
+   }
+    defer {
+        print("two")
+    }
+    defer {
+        print("trhree")
+    }
+   do {
+       print("txt")
+   }
+}
+
+//Decorator patter:
+/*
+The decorator pattern is a structural design pattern used in software development to dynamically add new functionality to an object without altering its structure.
+ 
+ It allows behavior to be added or removed from individual objects at runtime, providing greater flexibility than subclassing.
+ 
+ In iOS Swift, the decorator pattern can be implemented using a combination of protocols, classes, and inheritance. Here's a simplified example to illustrate how it works:
+ 
+ */
+
+//Protocal:
+
+protocol Component {
+    func operation () -> String
+}
+
+//Claass
+
+// ConcreteComponent implements the Component protocol
+class ConcreteComponent: Component {
+    func operation() -> String {
+        return "ConcreteComponent"
+    }
+}
+
+// Base class for decorators, conforms to the Component protocol
+class Decorator: Component {
+    let component: Component
+    
+    init(component: Component) {
+        self.component = component
+    }
+    
+    func operation() -> String {
+        return component.operation()
+    }
+}
+
+// ConcreteDecoratorA adds additional behavior to the Component
+class ConcreteDecoratorA: Decorator {
+    
+    override func operation() -> String {
+        return "ConcreteDecoratorA(" + super.operation() + ")"
+    }
+
+}
+
+// ConcreteDecoratorB adds different behavior to the Component
+class ConcreteDecoratorB: Decorator {
+    
+    override func operation() -> String {
+        return "ConcreteDecoratorB(" + super.operation() + ")"
+    }
+}
+
+//Usage
+
+let component = ConcreteComponent()
+component.operation()
+
+print(component.operation())
+
+let componentA = ConcreteDecoratorA(component: component)
+componentA.operation()
+print(componentA.operation())
+
+let componentB = ConcreteDecoratorB(component: componentA)
+componentB.operation()
+print(componentB.operation())
+
+
+//Class init() vs Convenice init()
+class PersonClass {
+    var name:String
+    var age : Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+  
+}
+
+extension PersonClass {
+    convenience init(name:String) {
+        self.init(name: name, age: 0 ) //Default value provided for comman value to all .
+    }
+}
+//Creating the instance of designted init()
+
+var personintObj = PersonClass(name: "Kanini", age: 33)
+
+debugPrint("Name:\(personintObj.name) and Age = \(personintObj.age)")
+
+//Creating the Instace of convenience init()
+
+var personObj = PersonClass(name: "Gogula")
+
+debugPrint("Name:\(personObj.name) and Age = \(personObj.age)")
+
+
+//Type Alias:
+
+/*
+ typealias is used to give an alternative name to an existing type.
+ 
+ */
+
+public typealias CustumString = String
+
+var data :CustumString = "Gogula"
+
+print(data)
+
+//Enum :
+
+enum Errortypes
+{
+    case jsonError
+    case ApiError
+    
+}
+var error: Errortypes = .ApiError
+
+switch error
+{
+case .ApiError:
+    
+    print("ApiError")
+case .jsonError:
+    print("jsonError")
+}
+
+//infix operator
+infix operator ***: MultiplicationPrecedence
+
+infix operator +++: AdditionPrecedence
+
+func +++(value1:Int, value2:Int) -> Int
+{
+   return value1 + value2
+}
+
+let result =   5  +++ 3
+print(result)
+
+func ***(value1:Int, value2:Int) -> Int
+{
+   return value1 * value2
+}
+
+let mutlipleresult =   5  *** 3
+print(mutlipleresult)
+
+
+
+//class with same instance :
+
+class MyPerson {
+    func value() -> Self {
+        return self
+    }
+}
+var obj = MyPerson()
+print(obj)
+var obj2 = obj.value()
+print(obj2)
+
+
+//Caseiterable
+
+enum Direction: CaseIterable
+{
+    case north
+    case east
+    case west
+    case south
+}
+
+print(Direction.allCases)
+//@discardableResult
+
+/*
+ @discardableResult attribute is used to indicate that the return value of a function can be ignored by the caller without generating a warning.
+ */
+
+
+
+
+
+//
+
+/*
+ @objc dynamic and @Persisted are both property wrappers used in RealmSwift to define properties of Realm model objects. However, they have some differences in their usage and behavior:
+
+-@objc dynamic is used to mark properties as dynamic, which allows Realm to observe and manage changes to these properties at runtime.
+ 
+ -@Persisted is a property wrapper introduced in newer versions of RealmSwift (such as RealmSwift 10.0.0 and above).
+ It serves a similar purpose to @objc dynamic in that it marks properties as persistent and managed by Realm, but it provides additional features and benefits:
+ */
