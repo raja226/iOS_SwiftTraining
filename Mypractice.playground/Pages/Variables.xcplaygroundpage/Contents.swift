dcb24993515d24879c7f9a584950e77aa9d18643
raja226 <iosrajasekhar@gmail.com>
2024-07-06 07:04:48 +0530

    variables types

--- Mypractice.playground/Pages/Variables.xcplaygroundpage/Contents.swift
+++ Mypractice.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -7,9 +7,9 @@ var greeting = "Hello, playground"
 print(greeting)
 
 
-var firstname,lastname,fullname:String // by using type antation
+var firstname,lastname,fullname:String? // by using type antation
 var age = 34 //type inferance
 let company = "Align" // its the constanct once you assign we con't modify or re-assign in swift .
-
+print(fullname ?? "null value ")
 
 
