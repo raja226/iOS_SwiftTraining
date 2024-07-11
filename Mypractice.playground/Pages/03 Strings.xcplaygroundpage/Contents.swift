86299ea1a5456ea1fc287c1463bb6bd930f1e279
raja226 <iosrajasekhar@gmail.com>
2024-07-11 15:25:02 +0530

    strings

--- Mypractice.playground/Pages/03 Strings.xcplaygroundpage/Contents.swift
+++ Mypractice.playground/Pages/03 Strings.xcplaygroundpage/Contents.swift
@@ -4,4 +4,25 @@ import Foundation
 
 var greeting = "Hello, playground"
 
-//: [Next](@next)
+var username:String = "Test"
+print(username)
+
+//concatenate;
+
+var firstName = "GOGULA"
+var lastName = "Rajasekhar"
+var fullname = firstName + " " + lastName
+print(fullname.lowercased())
+
+//contains
+fullname.contains("Raja")
+print(fullname.contains("Raja"))
+//fullname = ""
+if fullname.isEmpty
+{
+    print("Empty")
+
+}
+fullname.components(separatedBy: " ")
+
+print(fullname.components(separatedBy: " "))
