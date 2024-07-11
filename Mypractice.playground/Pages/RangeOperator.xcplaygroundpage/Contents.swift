9e9c11ded8903de584a16d16fadc6fa6f598bf68
raja226 <iosrajasekhar@gmail.com>
2024-07-11 11:02:47 +0530

    learning

---
+++ Mypractice.playground/Pages/RangeOperator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var myRange = 1...5
+
+print(myRange)
+dump(myRange)
+
+print(myRange.contains(3))
+print(myRange.contains(6))
--- Mypractice.playground/Pages/Variables.xcplaygroundpage/Contents.swift
+++ Mypractice.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -1,6 +1,7 @@
 //: [Previous](@previous)
 
 import Foundation
+import UIKit
 
 var greeting = "Hello, playground"
 
@@ -11,5 +12,25 @@ var firstname,lastname,fullname:String? // by using type antation
 var age = 34 //type inferance
 let company = "Align" // its the constanct once you assign we con't modify or re-assign in swift .
 print(fullname ?? "null value ")
+
+
+var result = 4 + 4
+print("result")
+print("result: \(result)")
+print("Reult:",result,"test",result)
+var worldstring = "gogla rajasekhar bala"
+print(String(format:"ttt:%d",result))
+debugPrint(worldstring)
+dump(result)
+
+struct Person {
+    var name:String
+    var age:Int
+}
+
+let personObj = Person(name: "rajasekhar", age: 85)
+
+dump(personObj)
+print(personObj)
 
 
