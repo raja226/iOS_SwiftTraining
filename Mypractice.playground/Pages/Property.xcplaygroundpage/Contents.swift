0e6007a94dbd19795ce493b0531833a6f2c9b70a
raja226 <iosrajasekhar@gmail.com>
2024-07-12 16:24:50 +0530

    mutating key

--- Mypractice.playground/Pages/Property.xcplaygroundpage/Contents.swift
+++ Mypractice.playground/Pages/Property.xcplaygroundpage/Contents.swift
@@ -37,3 +37,61 @@ obj.radius = 10.0
 print(obj.area)
 obj.area = 20
 print(obj.result!)
+
+class Multiplecation
+{
+    var num1 :Int = 5
+    var num2: Int = 5
+    
+    var mutiply: Int
+    {
+        get {
+            return num1 * num2
+        }
+    }
+    
+}
+let res = Multiplecation()
+print(res.mutiply)
+
+//observer : willset ,didset,
+
+class Observers
+{
+    var value: Int = 10
+    {
+        willSet(newValue)
+        {
+            print("willSet :\(newValue)")
+
+        }
+        didSet {
+            if value > oldValue
+            {
+                print("didset :\(value - oldValue)")
+            }
+        }
+    }
+}
+
+var observerObj = Observers()
+observerObj.value = 15
+observerObj.value = 25
+
+
+//method:metating
+
+struct Milk
+{
+    var products = 10
+    
+     mutating func productValueChange() ->Int {
+         products += products
+         
+         return products
+    }
+}
+
+var objmilk = Milk()
+var resultOfMethod = objmilk.productValueChange()
+print("products: \(resultOfMethod)")
