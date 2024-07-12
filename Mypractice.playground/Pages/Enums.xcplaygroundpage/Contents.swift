73c6a63bfa2c787ae549c7a38e808ed5d031e951
raja226 <iosrajasekhar@gmail.com>
2024-07-12 10:59:36 +0530

    enums

---
+++ Mypractice.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -0,0 +1,91 @@
+
+import Foundation
+import UIKit
+
+//Enum
+enum Marvel
+{
+    case Ironman
+    case Spiderman
+    case CaptainAmerica
+    case Blackpanther
+    case Thor
+}
+
+var hero = Marvel.Blackpanther
+switch(hero)
+{
+case .Ironman:
+    print("Ironman")
+case .Spiderman:
+    print("Spiderman")
+case .CaptainAmerica:
+    print("CaptainAmerica")
+case .Blackpanther:
+    print("Blackpanther")
+case .Thor:
+    print("Thor")
+    
+}
+
+enum Person
+{
+    case Name(String)
+    case Age(Int)
+}
+ 
+var name = Person.Name("Rajasekhar Swift")
+let age = Person.Age(33)
+
+switch(name)
+{
+case .Name(let pname):
+    print(pname)
+case .Age(let pAge):
+    print(pAge)
+}
+//With raw Value :
+
+enum Planet: Int {
+  
+    case mercury = 1
+    case earth = 2
+    case venus = 3
+}
+
+var planetObj = Planet.earth.rawValue
+
+print(planetObj)
+//recursion Enums :
+
+
+//interview :
+
+enum Result
+{
+    case success(data:String)
+    case failure(error:String)
+}
+
+func fetchApiData(completion: (Result) -> Void)
+{
+    let success = false
+    
+    if success {
+        completion(.success(data: "fetch API success"))
+    }else{
+        completion(.failure(error: "API error"))
+    }
+}
+
+fetchApiData { result in
+    
+    switch result
+    {
+    case .success(let messgage):
+        print(messgage)
+        
+    case .failure(let errormessage):
+        print(errormessage)
+    }
+}
